2a4ef8e7d41db0d5975332b8b13c1aaf64451c10
simone bresciani <s.bresciani026@studenti.unibs.it>
2021-05-04 22:33:30 +0200

    Abbozzo TestEquilibrio: bisogna rivedere l'assegnazione dell'ultima cifra di ciascuna riga

--- src/it/unibs/pgArnaldo/tamagolem/TestEquilibrio.java
+++ src/it/unibs/pgArnaldo/tamagolem/TestEquilibrio.java
@@ -10,30 +10,60 @@ public class TestEquilibrio {
     public static void main(String[] args) {
 
         int[][] mat = new int[N][N];
-        for(int i=0; i<N; i++){ //righe
+        for(int i=0; i<N-2; i++){ //righe tranne l'ultima
             int w = VITA;
             int caselle_positive=0;
-            for(int j=i+1; j!=N-2; j++){ // si ferma a n-2 perchè l'ultimo numero non può essere random
-                Random rand = new Random();
-                int pos = rand.nextInt(N-j+1)+i;
-                if(mat[i][pos] == 0){
-                    if(caselle_positive <= (N-1)/2){
-                        mat[i][pos] = rand.nextInt(w)+1;
-                        w -= mat[i][pos];
-                        caselle_positive++;
-                    }else{
-                        mat[i][pos] = -rand.nextInt(VITA-w);
-                        w -= -mat[i][pos];
-                    }
+            int revers = 0;
+            int num = N;
+            int caselle_negative =0;
+            int somma_controllo_positivi=0;
+            int somma_controllo_negativi=0;
+            for(int n=0; n< N; n++){
+                if(mat[i][n] > 0) {
+                    somma_controllo_positivi += mat[i][n];
+                    caselle_positive += 1;
+                }
+                if(mat[i][n] < 0) {
+                    somma_controllo_negativi += mat[i][n];
+                    caselle_negative += 1;
                 }
             }
-        }
+            w -= somma_controllo_positivi;
+            revers += somma_controllo_negativi;
+            for(int j=i+1; j!=N; j++){ // itero per un numero di volte pari alle posizioni disponibili
+                Random rand = new Random();
+                int pos = rand.nextInt(N-i-1) +i+1;  //numero randomico che va da i+1 a N-1
+                if(mat[i][pos] == 0){
+                    if(caselle_positive < (N-1)/2){
+                        mat[i][pos] = mat[pos][i] = rand.nextInt(( w - (num-2)/2 ) -1  ) +1;
+                        w -= mat[i][pos];
+                        revers += mat[i][pos];
+                        caselle_positive++;
+                        num -= 2;
 
-        for (int i=0; i<N; i++){
-            for (int j=0; j<N; j++){
-                System.out.print("[" + mat[i][j] + "]");
+                    }else{
+                        if (caselle_negative == 1){       //regola l'ultima cella della riga
+                            mat[i][pos] = mat[pos][i]= -revers;
+                        }else {
+                                mat[i][pos] = mat[pos][i]=  -rand.nextInt(revers - (caselle_negative - 1)) - 1; //revers é la quantitá positiva somma delle casellle giá assegnate in quella riga;
+                                revers += mat[i][pos];
+                                caselle_negative -= 1;
+                             }
+                    }
+                }else j -= 1; //se é una casella in cui ho giá scritto allora dovró randomizzare di nuovo la posizione e non contare l'iterazione del ciclo che é precisa per i cambiamenti da fare
+            }
+        }
+        //completamento ultima cella
+        for(int i=0; i< N-1; i++ ) {
+            mat[N - 2][N-1] = mat[N-1][N - 2] -= mat[N - 2][i];
+        }
+        //STAMPA
+        for (int a=0; a<N; a++){
+            for (int b=0; b<N; b++){
+                if (mat[a][b] < 0) System.out.printf("[" + mat[a][b] + "]" + " ");
+                else System.out.printf("[ " + mat[a][b] + "]" + " ");
             }
             System.out.print("\n");
-        }
+        }System.out.print("\n");
     }
 }
