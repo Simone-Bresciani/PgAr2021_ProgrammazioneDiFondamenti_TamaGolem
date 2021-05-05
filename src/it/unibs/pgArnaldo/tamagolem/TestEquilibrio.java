8d5c747257ab5a1618f137b5be51e7014a308cf6
simone bresciani <s.bresciani026@studenti.unibs.it>
2021-05-05 10:57:19 +0200

    aggiornamenti TestEquilibrio

--- src/it/unibs/pgArnaldo/tamagolem/TestEquilibrio.java
+++ src/it/unibs/pgArnaldo/tamagolem/TestEquilibrio.java
@@ -11,11 +11,10 @@ public class TestEquilibrio {
 
         int[][] mat = new int[N][N];
         for(int i=0; i<N-2; i++){ //righe tranne l'ultima
-            int w = VITA;
             int caselle_positive=0;
-            int revers = 0;
-            int num = N;
+            //int num = N;
             int caselle_negative =0;
+            int max_caselle_per_segno = (N-1)/2;  //se N é dispari(unica situazione da tenere in considerazione secondo me perché piú equilibrata)
             int somma_controllo_positivi=0;
             int somma_controllo_negativi=0;
             for(int n=0; n< N; n++){
@@ -28,26 +27,27 @@ public class TestEquilibrio {
                     caselle_negative += 1;
                 }
             }
-            w -= somma_controllo_positivi;
-            revers += somma_controllo_negativi;
+            int w = VITA - somma_controllo_positivi;
+            int revers = -somma_controllo_negativi + somma_controllo_positivi;
+
             for(int j=i+1; j!=N; j++){ // itero per un numero di volte pari alle posizioni disponibili
                 Random rand = new Random();
                 int pos = rand.nextInt(N-i-1) +i+1;  //numero randomico che va da i+1 a N-1
                 if(mat[i][pos] == 0){
-                    if(caselle_positive < (N-1)/2){
-                        mat[i][pos] = mat[pos][i] = rand.nextInt(( w - (num-2)/2 ) -1  ) +1;
+                    if (caselle_positive < max_caselle_per_segno){
+                        mat[i][pos] = mat[pos][i] = rand.nextInt(w -(max_caselle_per_segno - caselle_positive)) + 1;//(( w - (num-2)/2 ) - ) +1;
                         w -= mat[i][pos];
                         revers += mat[i][pos];
                         caselle_positive++;
-                        num -= 2;
+                        //num -= 2;
 
                     }else{
-                        if (caselle_negative == 1){       //regola l'ultima cella della riga
+                        if (max_caselle_per_segno - caselle_negative == 1){       //regola l'ultima cella della riga
                             mat[i][pos] = mat[pos][i]= -revers;
                         }else {
-                                mat[i][pos] = mat[pos][i]=  -rand.nextInt(revers - (caselle_negative - 1)) - 1; //revers é la quantitá positiva somma delle casellle giá assegnate in quella riga;
+                                mat[i][pos] = mat[pos][i]=  -rand.nextInt(revers - (max_caselle_per_segno-caselle_negative)) - 1; //revers é la quantitá positiva somma delle casellle giá assegnate in quella riga;
                                 revers += mat[i][pos];
-                                caselle_negative -= 1;
+                                caselle_negative ++;
                              }
                     }
                 }else j -= 1; //se é una casella in cui ho giá scritto allora dovró randomizzare di nuovo la posizione e non contare l'iterazione del ciclo che é precisa per i cambiamenti da fare
