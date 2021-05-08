c40c8825578c989af3747aeccdf5d4f2d932ddde
S1nfra <82461468+S1nfra@users.noreply.github.com>
2021-05-08 10:41:16 +0200

    è solo un inizio devo ancora sistemare

---
+++ src/it/unibs/pgArnaldo/tamagolem/Squadra.java
@@ -0,0 +1,16 @@
+package it.unibs.pgArnaldo.tamagolem;
+
+import java.util.ArrayList;
+
+public class Squadra
+{
+    private ArrayList<Tamagolem> lista_tamagolem = new ArrayList<Tamagolem>(); //lista per i tamagolem
+        lista_tamagolem.add();
+        lista_tamagolem.remove();
+
+
+    private ArryList<Pietre> lista_pietre = new ArryList<Pietre>();       //lista per il numero di pietre
+        lista_pietre.add();
+        lista_tamagolem.remove();
+
+}
