236377a9d53ae809896cfd6a1c2977f737e6addd
Your Name <f.dagani002@studenti.unibs.it>
2021-05-08 11:16:30 +0200

    Aggiunta libreria myLib di Serina

---
+++ src/it/unibs/pgArnaldo/tamagolem/Costanti.java
@@ -0,0 +1,7 @@
+package it.unibs.pgArnaldo.tamagolem;
+
+public class Costanti {
+
+    //fine del programma
+    public static final String END = "Scontro terminato";
+}
--- src/it/unibs/pgArnaldo/tamagolem/Main.java
+++ src/it/unibs/pgArnaldo/tamagolem/Main.java
@@ -1,7 +1,13 @@
 package it.unibs.pgArnaldo.tamagolem;
 
 public class Main {
+
     public static void main(String[] args) {
 
+        int rivincita = 0;
+        do{
+
+        }while(rivincita == 1);
+        System.out.println(Costanti.END);
     }
 }
