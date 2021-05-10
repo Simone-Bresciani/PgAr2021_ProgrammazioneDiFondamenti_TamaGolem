cc724e69fce1d2a5802cdd35ab3879c03fb35466
Your Name <f.dagani002@studenti.unibs.it>
2021-05-10 14:33:59 +0200

    Inzio lavori main

--- src/it/unibs/pgArnaldo/tamagolem/Costanti.java
+++ src/it/unibs/pgArnaldo/tamagolem/Costanti.java
@@ -2,6 +2,8 @@ package it.unibs.pgArnaldo.tamagolem;
 
 public class Costanti {
 
+    public static final String LIVELLO = "Inscerisci il livello di difficolta della partità: 1-easy 2-medium 3-difficult 4-god";
+
     //fine del programma
     public static final String END = "Scontro terminato";
 }
--- src/it/unibs/pgArnaldo/tamagolem/Main.java
+++ src/it/unibs/pgArnaldo/tamagolem/Main.java
@@ -1,11 +1,14 @@
 package it.unibs.pgArnaldo.tamagolem;
 
+import mylib.InputDati;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        int rivincita = 0;
+        int rivincita = 0; // variabile per far ripartire da capo lo scontro in caso si voglia rigiocare
         do{
+            int livello = InputDati.leggiIntero(Costanti.LIVELLO);
 
         }while(rivincita == 1);
         System.out.println(Costanti.END);
